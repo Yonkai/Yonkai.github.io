4220757ca734be280c42922e2741f6617cec7f82
Frank <Yonkai@users.noreply.github.com>
2017-04-23 19:36:55 -0400

    Update ShowAbout.js
    add info about me

--- ShowAbout.js
+++ ShowAbout.js
@@ -8,7 +8,12 @@ $(document).ready(function(){
           $(".toggle, .textForSubheadingsHidden").toggleClass("toggle textForSubheadingsHidden");
       // Multiline String using EC6 Backtics.
          $(".innerText").html(` <p>
-            About me... TODO </br> </br>     
+    Hey! I'm Frank, I have recently fell in love <br>
+with programming and web development in particular. I <br> 
+have been learning and applying it seriously for <br> 
+about 7 months now.I am currently studying Computer <br>
+Networking Technology and Computer Scence, I am <br> 
+set to graduate with those two associates <br>in a year.
         </p>`);
     });
 });
