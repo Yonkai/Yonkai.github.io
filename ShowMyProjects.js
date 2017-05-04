d99d2f1e872860899291d0cc65cbb14a9dd7b279
Frank <Yonkai@users.noreply.github.com>
2017-05-04 06:37:02 -0400

    Fixed hyperlinks

--- ShowMyProjects.js
+++ ShowMyProjects.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
        $(".toggle, .textForSubheadingsHidden").toggleClass("toggle textForSubheadingsHidden");
         // Multiline String using EC6 Backtics.
          $(".innerText").html(` <p>
-            <a href="yonkai.github.io/Clue emulator">"Cat, Dog, Bird":</a><br/><br/>
+            <a href="/CDB">"Cat, Dog, Bird":</a><br/><br/>
             This is a basic website using HTML and CSS that implements the basic principles of responsive<br/>
             design.<br /><br/>
             Stack: HTML,CSS
             <br /><br/>
             
-	        <a href="yonkai.github.io/Clue emulator">"Clue Scroll Emulator":</a><br/><br/>
+	        <a href="/Clue emulator">"Clue Scroll Emulator":</a><br/><br/>
             Emulator for an online video game that models in-game reward generation<br/>
             into real money.<br /><br/>
             Stack: HTML, CSS, Vanilla JS, JQuery, Responsive Design
