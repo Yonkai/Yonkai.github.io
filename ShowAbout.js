60b7fdb12376a9e3fef1c30129011e8b9f002279
Frank Gunsch <Yonkai@users.noreply.github.com>
2017-05-04 18:51:15 -0400

    Update ShowAbout.js

--- ShowAbout.js
+++ ShowAbout.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
     Hey! I'm Frank, I have recently fell in love <br>
 with programming and web development in particular. I <br> 
 have been learning and applying it seriously for <br> 
-about 7 months now.I am currently studying Computer <br>
-Networking Technology and Computer Scence, I am <br> 
+about 7 months now. I am currently studying Computer <br>
+Networking Technology and Computer Science, I am <br> 
 set to graduate with those two associates <br>in a year.
         </p>`);
     });
