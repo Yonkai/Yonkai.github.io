c10e9d565b208231dc8d2a9d40b35e07e7c5237d
Frank <fgunsch.webdev@gmail.com>
2017-09-27 21:28:51 -0400

    nonsense

--- ShowContact.js
+++ ShowContact.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $(".contact").click(function(){
-       $(".sub-header").eq(1).children().toggle();
-       $(".sub-header").eq(2).children().toggle();
-       $(".sub-header").eq(3).children().toggle();
+
        $(".project-names").children().toggle();
        $(".projects").text("Contact");
        $(".toggle, .textForSubheadingsHidden").toggleClass("toggle textForSubheadingsHidden");
